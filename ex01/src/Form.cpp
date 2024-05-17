b0b400f8dc45bfc8659c1a88c1bca7e1702bc3a1
Shetteemah <shettimaali58@yahoo.com>
2024-05-17 02:34:57 +0200

    ex01: Form.

--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -1,14 +1,16 @@
 #include "Form.hpp"
 
-        Form();
-        Form(const std::string &name);
-        Form(int signedGrade, int execGrade);
-        Form(const std::string &name, int signedGrade, int execGrade);
-        Form(const Form &src);
-        ~Form();
-        Form const &operator=(const Form &src);
-        const std::string getName() const;
-        int getSignedGrade() const;
-        int getExecGrade() const;
-        int getSignedState() const;
-        void beSigned(const Bureaucrat *bureau);
+Form::Form() {}
+Form::Form(const std::string &name) {}
+Form::Form(int signedGrade, int execGrade) {}
+Form::Form(const std::string &name, int signedGrade, int execGrade) {}
+Form::Form(const Form &src) {}
+Form::~Form() {}
+Form const &Form::operator=(const Form &src) {}
+const std::string Form::getName() const {}
+int Form::getSignedGrade() const {}
+int Form::getExecGrade() const {}
+int Form::getSignedState() const {}
+void Form::beSigned(const Bureaucrat *bureau) {}
+const char *Form::GradeTooHighException::what() const throw() {}
+const char *Form::GradeTooHighException::what() const throw() {}
