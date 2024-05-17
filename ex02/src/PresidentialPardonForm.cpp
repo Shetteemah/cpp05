ffa4c5943bb5dee974389caa674706ad5eeaf261
Shetteemah <shettimaali58@yahoo.com>
2024-05-17 03:33:14 +0200

    ex02: forms implementation.

--- ex02/src/PresidentialPardonForm.cpp
+++ ex02/src/PresidentialPardonForm.cpp
@@ -1,9 +1,58 @@
 #include "PresidentialPardonForm.hpp"
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
 
-        // PresidentialPardonForm();
-        // PresidentialPardonForm(const std::string &target);
-        // PresidentialPardonForm(const PresidentialPardonForm &src);
-        // ~PresidentialPardonForm();
-        // PresidentialPardonForm const &operator=(const PresidentialPardonForm &src);
-        // void execute(const Bureaucrat &exec) const;
-        // const std::string getTarget() const;
+PresidentialPardonForm::PresidentialPardonForm() : AForm("Presidential Pardon Form", 25, 5), _target("default") 
+{
+    std::cout << "PresidentialPardonForm: " << this->getName() << " created!" << std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target) : AForm("Presidential Pardon Form", 25, 5), _target(target)
+{
+    if (this->getSignedGrade() < 1 || this->getExecGrade() < 1)
+        throw GradeTooHighException();
+    else if (this->getSignedGrade() > 150 || this->getExecGrade() > 150)
+        throw GradeTooLowException();
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &src) : AForm("Presidential Pardon Form", 25, 5), _target(src.getTarget())
+{
+    std::cout << "PresidentialPardonForm: copy constructor initiated" << std::endl;
+    std::cout << src.getName() << " Successfully copied to " << this->getName() << "!" << std::endl;
+    *this = src;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+    std::cout << "PresidentialPardonForm: " << this->getName() << " destroyed!" << std::endl;
+}
+
+PresidentialPardonForm const &PresidentialPardonForm::operator=(const PresidentialPardonForm &src)
+{
+    std::cout << "PresidentialPardonForm: assignment operator initiated" << std::endl;
+    this->AForm::operator=(src);
+    return (*this);
+}
+
+void PresidentialPardonForm::execute(const Bureaucrat &exec) const
+{
+    if (exec.getGrade() > this->getExecGrade())
+        throw GradeTooLowException();
+    else
+        std::cout << this->getTarget() << " has been pardoned by Zafod Beeblebrox!" << std::endl;
+}
+
+const std::string PresidentialPardonForm::getTarget() const
+{
+    return (this->_target);
+}
+
+std::ostream &operator<<(std::ostream &os, const PresidentialPardonForm &form)
+{
+    os << form.getName() << " Details" << std::endl;
+    os << "Form grade: " << form.getSignedGrade();
+    os << "\nSigned state: " << (form.getSignedState() ? "true" : "false");
+    os << "\nExecution grade: " << form.getExecGrade();
+    os << "\nSigned grade: " << form.getSignedGrade() << std::endl;
+    return (os);
+}
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,5 +1,8 @@
 #include "../include/Bureaucrat.hpp"
-
+#include "../include/AForm.hpp"
+#include "../include/PresidentialPardonForm.hpp"
+#include "../include/RobotomyRequestForm.hpp"
+#include "../include/ShrubberyCreationForm.hpp"
 int main()
 {
     std::cout << "\n" << std::endl;
@@ -10,6 +13,13 @@ int main()
         std::cout << midRank << std::endl;
         Bureaucrat lowRank("LowRank", 150);
         std::cout << lowRank << std::endl;
+
+        PresidentialPardonForm form1("Target1");
+        std::cout << form1 << std::endl;
+        RobotomyRequestForm form2("Target2");
+        std::cout << form2 << std::endl;
+        ShrubberyCreationForm form3("Target3");
+        std::cout << form3 << std::endl;
 
         std::cout << "\n" << std::endl;
     }
